d01b67317f60233964246bcf27774da7b490b7e7
cnyxq <754527700@qq.com>
2019-08-12 11:32:05 +0800

    第一个Vue小项目

--- src/components/goods/goodslist.vue
+++ src/components/goods/goodslist.vue
@@ -1,11 +1,62 @@
 <template>
-    <div>goodslist</div>
+    <div class="goods-list">
+        <div class="goods-item" v-for="item in prodlist" :key="item.img_url">
+            <img :src="item.img_url" alt="">
+            <h1>{{item.title}}</h1>
+            <div class="info">
+                <div class="price">
+                    <span class="now">{{item.sell_price}}</span>
+                    <span class="old">{{item.market_price}}</span>
+                </div>
+                <div class="sell">
+                    <span>热卖中</span>
+                    <span>剩{{item.stock_quantity}}件</span>
+                </div>
+            </div>
+        </div>
+        <mt-button type="danger" size="large" plain @click="getMore">加载更多</mt-button>
+    </div>
 </template>
 <script>
     export default {
-
+      data() {
+        return {
+          pageIndex: 1,
+          prodlist: []
+        }
+      },
+      created() {
+        this.getProdList()
+      },
+      methods: {
+        getProdList() {
+          this.axios.get("http://www.liulongbin.top:3005/api/getgoods/"+"?pageindex="+this.pageIndex).then(result => {
+            console.log(result)
+            if(result.data.status === 0){
+              this.prodlist = this.prodlist.concat(result.data.message)
+            }else{
+              Toast("获取数据失败！")
+            }
+          })
+        },
+        getMore() {
+          this.pageIndex++
+          this.getProdList()
+        }
+      }
     }
 </script>
 <style>
+    .goods-list .goods-item {
+        width: 45%;
+        display: inline-block;
+        box-shadow: 0 0 10px #999;
+    }
+    .goods-list .goods-item img{
+        width: 100%;
+    }
+    .goods-list .goods-item h1{
+        font-size: 16px;
+    }
 
 </style>
