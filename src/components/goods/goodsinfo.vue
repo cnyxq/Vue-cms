30cf09b074526cd56f6b2d71332d67ef9a51e9eb
cnyxq <754527700@qq.com>
2019-08-14 12:20:07 +0800

    第一个Vue小项目

--- src/components/goods/goodsinfo.vue
+++ src/components/goods/goodsinfo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mui-content">
+    <div class="mui-content" >
         <div class="mui-card">
             <div class="mui-card-content">
                 <div class="mui-card-content-inner">
@@ -11,20 +11,16 @@
                 </div>
             </div>
         </div>
-        <div class="mui-card" v-for="(item,index) in goodsInfo" :key="index">
-            <div class="mui-card-header">{{item.title}}</div>
+        <div class="mui-card">
+            <div class="mui-card-header">{{goodsInfo.title}}</div>
             <div class="mui-card-content">
                 <div class="mui-card-content-inner">
                     <div class="price">
-                        市场价：<span class="market">{{item.market_price}}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        销售价：<span class="sell">{{item.sell_price}}</span>
+                        市场价：<span class="market">{{goodsInfo.market_price}}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        销售价：<span class="sell">{{goodsInfo.sell_price}}</span>
                     </div>
-                    <p>购买数量：</p>
-                    <div class="mui-numbox">
-                        <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-                        <input class="mui-input-numbox" type="number" value="1">
-                        <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
-                    </div>
+                    <p class="num">购买数量：</p>
+                    <numbox></numbox>
                     <div class="button">
                         <mt-button type="primary">立即购买</mt-button>
                         <mt-button type="danger">加入购物车</mt-button>
@@ -37,79 +33,34 @@
             <div class="mui-card-header">商品参数</div>
             <div class="mui-card-content">
                 <div class="mui-card-content-inner">
-                    包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）
-                </div>
-            </div>
-            <div class="mui-card-footer">页脚</div>
-        </div>
-
-        <div class="mui-card">
-            <div class="mui-card-header mui-card-media" style="height:40vw;"></div>
-            <div class="mui-card-content">
-                <div class="mui-card-content-inner">
-                    <p>Posted on January 18, 2016</p>
-                    <p style="color: #333;">这里显示文章摘要，让读者对文章内容有个粗略的概念...</p>
+                    <p>商品货号：{{goodsInfo.goods_no}}</p>
+                    <p>库存情况：{{goodsInfo.stock_quantity}}</p>
+                    <p>上架时间：{{goodsInfo.add_time | dateFormat}}</p>
                 </div>
             </div>
             <div class="mui-card-footer">
-                <a class="mui-card-link">Like</a>
-                <a class="mui-card-link">Read more</a>
-            </div>
-        </div>
-
-
-        <!--<div class="mui-card">
-            <div class="mui-card-header mui-card-media">
-                <div class="mui-card-media-object mui-pull-left">
-                    <img src="../images/logo.png" width="34px" height="34px" />
-                </div>
-                <div class="mui-card-media-body">
-                    小M
-                    <p class="mui-ellipsis">发表于 6小时前</p>
-                </div>
-            </div>
-            <div class="mui-card-content mui-card-media" style="height:40vw;background-image:url(../images/yuantiao.jpg)"></div>
-            <div class="mui-card-footer">
-                <a class="mui-card-link">Like</a>
-                <a class="mui-card-link">Comment</a>
-                <a class="mui-card-link">Read more</a>
-            </div>
-        </div>-->
-
-        <div class="mui-card">
-            <div class="mui-card-header mui-card-media">
-                <img src="">
-                <div class="mui-media-body">
-                    小M
-                    <p>发表于 2016-06-30 15:30</p>
-                </div>
-                <!--<img class="mui-pull-left" src="../images/logo.png" width="34px" height="34px" />
-                <h2>小M</h2>
-                <p>发表于 2016-06-30 15:30</p>-->
-            </div>
-            <div class="mui-card-content">
-                <img src="" alt="" width="100%">
-            </div>
-            <div class="mui-card-footer">
-                <a class="mui-card-link">Like</a>
-                <a class="mui-card-link">Comment</a>
-                <a class="mui-card-link">Read more</a>
+                <mt-button type="primary" size="large" plain>图文介绍</mt-button>
+                <mt-button type="danger" size="large" plain>商品评论</mt-button>
             </div>
         </div>
     </div>
 </template>
 <script>
+    import numbox from '../subcomponents/goodsinfo_numbox.vue'
     export default {
       data() {
         return {
           id: this.$route.params.id,
           imageList: [],
-          goodsInfo: [],
+          goodsInfo: {},
         }
       },
       created() {
         this.getData()
         this.getInfo()
+      },
+      mounted() {
+
       },
       methods: {
         getData() {
@@ -124,16 +75,22 @@
         getInfo() {
           this.axios.get("http://www.liulongbin.top:3005/api/goods/getinfo/"+this.id).then(result => {
             if(result.data.status === 0){
-              this.goodsInfo = result.data.message
+              this.goodsInfo = result.data.message[0]
             }else{
               Toast("获取数据失败！")
             }
           })
         }
       },
+      components: {
+        "numbox": numbox
+      }
     }
 </script>
-<style>
+<style scoped>
+    .mui-content {
+        overflow: hidden;
+    }
     .mint-swipe {
         height: 200px;
         text-align: center;
@@ -141,8 +98,21 @@
     .mint-swipe img {
         height: 100%;
     }
-    .mui-card .mui-card-content-inner p {
+    .mui-card .mui-card-content-inner .num {
         font-size: 15px;
         display: inline-block;
+    }
+    .mui-card-footer {
+        display: block;
+    }
+    .mint-button {
+        margin: 10px 0;
+    }
+    .market {
+        text-decoration: line-through;
+    }
+    .sell {
+        font-size: 20px;
+        color: red;
     }
 </style>
---
+++ src/components/subcomponents/goodsinfo_numbox.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mui-numbox" data-numbox-min='1' data-numbox-max='100'>
+        <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+        <input class="mui-input-numbox" type="number" value="1">
+        <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+</template>
+<script>
+    import mui from '../../lib/mui/js/mui'
+    export default {
+      data() {
+        return {
+
+        }
+      },
+      mounted() {
+        mui(".mui-numbox").numbox()
+      },
+      methods: {
+
+      }
+    }
+</script>
+<style>
+
+</style>
